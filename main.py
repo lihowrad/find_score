c146997ed2e1ea6b2d891d846ec609d9d58068ea
lihowrad <59283577+lihowrad@users.noreply.github.com>
2021-12-26 22:02:55 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -27,71 +27,30 @@ while count != int(setting.get('DURATION')) + 1:
     count += 1
     data = pd.read_csv('result/export.csv')
     result = data.iloc[-1, 0]
-    if math.isnan(result):
-        print('开始尝试查询成绩')
-        os.system('python scripts/sign-in.py')
-        break
-    if count != setting.get('DURATION') + 1:
-        time.sleep(setting.get('DELAY_TIME'))
+    if not isinstance(result, str):
+        if math.isnan(result):
+            print('开始尝试查询成绩')
+            os.system('python scripts/sign-in.py')
+            break
+        if count != setting.get('DURATION') + 1:
+            time.sleep(setting.get('DELAY_TIME'))
 
-duration = 1000  # millisecond
-freq = 440  # Hz
-winsound.Beep(freq, duration)
-print('程序执行完成,结果成功输出')
-score = list(pd.read_csv('result/score.csv').iloc[:, 0])
-score_dir = {'政治': score[0], '英语': score[1], '数学': score[2], '专业课': score[3], '总分': score[4]}
-# score_dir = {'政治': 61, '英语': 84, '数学': 130, '专业课': 119, '总分': 60+84+130+119}
+try:
+    score = list(pd.read_csv('result/score.csv').iloc[:, 0])
+    duration = 1000  # millisecond
+    freq = 440  # Hz
+    winsound.Beep(freq, duration)
+    print('程序执行完成,结果成功输出')
+    score_dir = {'政治': score[0], '英语': score[1], '数学': score[2], '专业课': score[3], '总分': score[4]}
+    # score_dir = {'政治': 61, '英语': 84, '数学': 130, '专业课': 119, '总分': 60+84+130+119}
 
-# ******************************** 这里是自定义内容 *******************************************
-# 现在成绩已经保存在了score_dir中, 您希望如何查看成绩, 请调整以下代码
-time.sleep(3)
-os.system('cls')
-print('我已经偷偷帮您看了眼成绩~')
-# 如果您想直接查看成绩
-if style == 1:
-    print('您的成绩为')
-    print('政治:', score_dir['政治'])
-    print('英语:', score_dir['英语'])
-    print('数学', score_dir['数学'])
-    print('专业课', score_dir['专业课'])
-    print('总分', score_dir['总分'])
-
-# 如果您希望不直接获得成绩, 首先获得一些成绩相关的信息
-if style == 2:
-    time.sleep(1)
-    print('我来看看您和目标分数差多少')
-    time.sleep(2)
-    difference = int(score_dir['总分']) - ori_score
-    if difference < 0:
-        print('嗯~~您发挥的没有想象的好呢, 是否继续看下去?')
-        time.sleep(2)
-        a = input("输入1代表继续看下去,输入0代表放弃看下去,输入后按回车进行下一步:")
-        if int(a) == 1:
-            if difference < acceptable_score_difference:
-                print('您发挥的真的不太好呢, 是否还要继续看')
-                time.sleep(2)
-                b = input("输入1代表继续看下去,输入0代表放弃看下去,输入后按回车进行下一步:")
-                if int(b) == 1:
-                    print('您的成绩为')
-                    print('政治:', score_dir['政治'])
-                    print('英语:', score_dir['英语'])
-                    print('数学', score_dir['数学'])
-                    print('专业课', score_dir['专业课'])
-                    print('总分', score_dir['总分'])
-                else:
-                    print('请不要太难过,人生无处不青山')
-            else:
-                print('虽然没有达到预期, 但是您和预期差的不多呢, 您的成绩为:')
-                print('政治:', score_dir['政治'])
-                print('英语:', score_dir['英语'])
-                print('数学', score_dir['数学'])
-                print('专业课', score_dir['专业课'])
-                print('总分', score_dir['总分'])
-        else:
-            print('请不要太难过,人生无处不青山')
-    else:
-        print('嗯~~您发挥的和预期一样好呢')
-        time.sleep(2)
+    # ******************************** 这里是自定义内容 *******************************************
+    # 现在成绩已经保存在了score_dir中, 您希望如何查看成绩, 请调整以下代码
+    time.sleep(3)
+    os.system('cls')
+    print('我已经偷偷帮您看了眼成绩~')
+    # 如果您想直接查看成绩
+    if style == 1:
         print('您的成绩为')
         print('政治:', score_dir['政治'])
         print('英语:', score_dir['英语'])
@@ -99,9 +58,54 @@ if style == 2:
         print('专业课', score_dir['专业课'])
         print('总分', score_dir['总分'])
 
-if style == 3:
-    # 请在此处使用您希望获得成绩的方法
-    # 可能有用的提醒:score_dir以字典的形式储存着您的成绩, 其中字典的键包括"政治""英语""数学""专业课""总分", 字典的值是字符串形式(重要), 不要忘记用int转换
+    # 如果您希望不直接获得成绩, 首先获得一些成绩相关的信息
+    if style == 2:
+        time.sleep(1)
+        print('我来看看您和目标分数差多少')
+        time.sleep(2)
+        difference = int(score_dir['总分']) - ori_score
+        if difference < 0:
+            print('嗯~~您发挥的没有想象的好呢, 是否继续看下去?')
+            time.sleep(2)
+            a = input("输入1代表继续看下去,输入0代表放弃看下去,输入后按回车进行下一步:")
+            if int(a) == 1:
+                if difference < acceptable_score_difference:
+                    print('您发挥的真的不太好呢, 是否还要继续看')
+                    time.sleep(2)
+                    b = input("输入1代表继续看下去,输入0代表放弃看下去,输入后按回车进行下一步:")
+                    if int(b) == 1:
+                        print('您的成绩为')
+                        print('政治:', score_dir['政治'])
+                        print('英语:', score_dir['英语'])
+                        print('数学', score_dir['数学'])
+                        print('专业课', score_dir['专业课'])
+                        print('总分', score_dir['总分'])
+                    else:
+                        print('请不要太难过,人生无处不青山')
+                else:
+                    print('虽然没有达到预期, 但是您和预期差的不多呢, 您的成绩为:')
+                    print('政治:', score_dir['政治'])
+                    print('英语:', score_dir['英语'])
+                    print('数学', score_dir['数学'])
+                    print('专业课', score_dir['专业课'])
+                    print('总分', score_dir['总分'])
+            else:
+                print('请不要太难过,人生无处不青山')
+        else:
+            print('嗯~~您发挥的和预期一样好呢')
+            time.sleep(2)
+            print('您的成绩为')
+            print('政治:', score_dir['政治'])
+            print('英语:', score_dir['英语'])
+            print('数学', score_dir['数学'])
+            print('专业课', score_dir['专业课'])
+            print('总分', score_dir['总分'])
+
+    if style == 3:
+        # 请在此处使用您希望获得成绩的方法
+        # 可能有用的提醒:score_dir以字典的形式储存着您的成绩, 其中字典的键包括"政治""英语""数学""专业课""总分", 字典的值是字符串形式(重要), 不要忘记用int转换
+        pass
+except FileNotFoundError:
     pass
 
 print('本程序已经全部完成,希望能够给予您一点点小小的帮助')
